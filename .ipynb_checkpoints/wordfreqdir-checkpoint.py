2fa5ad105fc563af217f38e85710314bccfb33c1
amnet@gmail.com <amnet04@gmail.com>
2020-10-26 19:16:27 -0600

    multiprocess freqdict funcionando

--- .ipynb_checkpoints/wordfreqdir-checkpoint.py
+++ .ipynb_checkpoints/wordfreqdir-checkpoint.py
@@ -1,23 +1,23 @@
 import os
 import multiprocessing as mp
-import collections, functools, operator
 from itertools import repeat
-from line_chunker import line_counter, chunker
+from .chunkers import by_position_chunker
+from nltk.probability import FreqDist
+from nltk import word_tokenize
+import collections, functools, operator
 import pickle
+import time
 
+def wordfrecdict(sentence_list):
+    fdict =  FreqDist()
+    for sentence in sentence_list[1]:
+        for word in word_tokenize(sentence):
+            fdict[word] += 1
+    return fdict
+            
+            
 
-def wordfreqdict(input_t):
-    wf_dict = {}
-    if input_t[2]:
-        for t in input_t[2].split():
-            try:
-                wf_dict[t] += 1
-            except:
-                wf_dict[t] = 1
-        return wf_dict
-    
-    
-def mprocess_wordfrecdict(input_file, output_file, turns, workers=False):
+def mprocess_wordfrecdict(input_file, output_file, turns, workers=False, min_chunk_size_bytes=8388608):
     """ Parallel text preprocess for big text files.
 
     Args:
@@ -27,8 +27,10 @@ def mprocess_wordfrecdict(input_file, output_file, turns, workers=False):
         turns (TYPE): Number of cycles (More for less ram usage, more io operations)
         workers (bool, optional): Number of CPUs to use.
     """
-    print(f"Empecé con {input_file}")
-    lines_in_file = line_counter(input_file)
+    #print(f"Empecé con {input_file}")
+    #lines_in_file = line_counter(input_file)
+    
+    file_size = os.path.getsize(input_file)
 
     if 'sched_getaffinity' in dir(os):
         MAX_WORKERS = len(os.sched_getaffinity(0))
@@ -43,21 +45,35 @@ def mprocess_wordfrecdict(input_file, output_file, turns, workers=False):
 
     if workers > MAX_WORKERS:
         workers = MAX_WORKERS
-
-    if lines_in_file <= workers:
-        workers = lines_in_file
+        
+    if file_size > min_chunk_size_bytes*workers*turns: # El tamaño del archivo es mas grande que el minimo
+        chunk_size = int(round(file_size/(workers*turns)))
+        
+    elif file_size > min_chunk_size_bytes*workers: # Ajustar turnos
+        turns = int(round(file_size/(min_chunk_size_bytes*workers)))
+        if turns == 0:
+            turns = 1
+        chunk_size = int(round(file_size/(workers*turns)))
+    
+    else: # Ajustar workers
         turns = 1
+        workers = int(round(file_size)/min_chunk_size_bytes)
+        if workers == 0:
+            workers = 1
+        chunk_size = int(round(file_size/(workers)))
 
-    if lines_in_file <= workers * turns:
-        turns = int(lines_in_file / workers)
+    freq_dist = FreqDist()
+    for enum, chunk in enumerate(by_position_chunker(input_file, workers, turns)):
+        percentage = enum / turns * 100
+        print(f"Ciclo:{enum+1} de {turns}, {round(percentage)}%, Procesando {input_file}", end="\r", flush=True)
+        with mp.Pool(workers) as p:
+            partial_freq_dist = FreqDist(functools.reduce(operator.add, p.map(wordfrecdict, chunk)))
+            freq_dist += partial_freq_dist
+    return freq_dist    
+        
+        
 
-    lines_per_turn = int(round(lines_in_file / turns))
-
-    lines_per_workerturn = int(round(lines_per_turn / workers))
-
-    results = []
-
-    with mp.Manager() as manager:
+    """with mp.Manager() as manager:
         wf_dict = manager.dict()
         with mp.Pool(workers) as p:
             for enum, chunk in enumerate(chunker(input_file, lines_per_turn)):
@@ -76,7 +92,7 @@ def mprocess_wordfrecdict(input_file, output_file, turns, workers=False):
             print(f"Termine este ciclo, voy pal otro, Procesando {input_file}")
     results = dict(functools.reduce(operator.add, map(collections.Counter, results)))
     save_in_file(results, output_file)
-    print(f"Final final no va más :), Procesando {input_file}")
+    print(f"Final final no va más :), Procesando {input_file}")"""
 
 
 def save_in_file(input_dict, output_file):
